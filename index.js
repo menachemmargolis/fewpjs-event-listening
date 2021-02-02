d0544f00c2e50fe73528bb0c62e76a75978a4f86
menachem margolis <margolismendel5728@gmail.com>
2021-02-02 18:59:44 -0500

    commit tree f588d92f479e38038afff5bf4f22ec8fc34fb4f3

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-function addingEventListener() {
+document.querySelector("button").addEventListener("click", handleClick);
+
+function handleClick(e) {
+  const timeString = document.getElementById('time').value
+  sayHello(greet(timeString))
+}
+
+
+function sayHello(msg){
+   alert (msg)
+}
+
+function greet(timeStr){
+  let time = parseInt(timeStr)
+
+  if (time < 12){return "good morning"}
+  if (time > 18){return "good night"}
+  return "good afternoon"
 }
